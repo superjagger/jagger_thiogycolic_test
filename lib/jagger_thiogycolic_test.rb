4244a651446eb895fa0937bbf5b10c802f10e385
jagger <1336098842@qq.com>
2024-03-05 19:39:13 +0800

    first commit

---
+++ lib/jagger_thiogycolic_test.rb
@@ -0,0 +1,6 @@
+require "jagger_thiogycolic_test/version"
+
+module JaggerThiogycolicTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_thiogycolic_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerThiogycolicTest
+  VERSION = "0.1.1"
+end
